71685a674d41401711bb8d2819f98ea1099d21de
Swaraj Bawankule <95970767+SwarajBawankule@users.noreply.github.com>
2024-08-01 18:02:20 +0530

    server.js

--- server.js
+++ server.js
@@ -5,13 +5,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors(
-  {
-    origin: ["https://contact-application-flax.vercel.app/"],
-    METHODS: ["POSt", "GET"],
-    Credential : true
-  }
-));
+
 
 // Connect to MongoDB using environment variables
 mongoose.connect(process.env.MONGO_URI, {
